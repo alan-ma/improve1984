a1ddd200c4665c4fbe24f142e92a015923af793f
Alan Ma <alan@Energymac.local>
2017-01-09 01:01:00 -0500

    finished!
    main goals and elon added

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+var clicked = 0;
 function enter() {
 	$('#dystopia').css('transform', 'translateX(-100%)');
 	$('#utopia').css('transform', 'translateX(100%)');
@@ -5,6 +6,10 @@ function enter() {
 	setTimeout(function() {
 		$('#slide').css('display', 'none');
 		$('.content').css('display', 'block');
+		$('#elon').css('display', 'block');
+		setTimeout(function() {
+			$('.content').css('opacity', '1');
+		}, 5);
 	}, 2000);
 }
 function intro() {
@@ -14,4 +19,15 @@ $(document).ready(function(){
 	setTimeout(function() {
 		intro();
 	}, 1500);
+	$('#elon img').click(function() {
+		if (clicked == 0) {
+			$('.content').css('filter', 'brightness(40%)');
+			$('#tesla').slideDown();
+			clicked = 1;
+		} else {
+			$('.content').css('filter', 'brightness(100%)');
+			$('#tesla').slideUp();
+			clicked = 0;
+		}
+	});
 });
